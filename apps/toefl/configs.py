3eccbf7ae6e7fa0f2fab97f4b90bbd0f02e13ab0
Nicholas <nicholas.zhaoyu@gmail.com>
2013-10-23 21:22:41 -0700

    Add recording example.

--- apps/toefl/configs.py
+++ apps/toefl/configs.py
@@ -1,4 +1,6 @@
 from handlers.reading import ReadingHandler
+from handlers.speaking import SpeakingHandler
 
-handlers = [('/toefl/reading', ReadingHandler)]
+handlers = [('/toefl/reading', ReadingHandler),
+            ('/toefl/speaking', SpeakingHandler)]
 
