ee4b49da1e6e2236fbcbdbb7fc43d09d6834c24e
Nicholas <nicholas.zhaoyu@gmail.com>
2013-10-26 11:33:12 -0700

    Add data

--- apps/toefl/configs.py
+++ apps/toefl/configs.py
@@ -1,6 +1,8 @@
+from handlers.edit_reading import EditReadingHandler
 from handlers.reading import ReadingHandler
 from handlers.speaking import SpeakingHandler
 
 handlers = [('/toefl/reading', ReadingHandler),
-            ('/toefl/speaking', SpeakingHandler)]
+            ('/toefl/speaking', SpeakingHandler),
+            ('/toefl/edit_reading', EditReadingHandler)]
 
--- server.py
+++ server.py
@@ -4,10 +4,13 @@ import jinja2
 import os
 
 from handlers.index import IndexHandler
+from handlers.login import LoginHandler
 from handlers.debug import SystemInfoHandler
 from apps.toefl import configs as toefl_config
 
-handlers = [('/', IndexHandler), ('/debug', SystemInfoHandler)]
+handlers = [('/', IndexHandler), 
+            ('/debug', SystemInfoHandler),
+            ('/login', LoginHandler)]
 handlers = handlers + toefl_config.handlers
 
 logging.info(handlers)
