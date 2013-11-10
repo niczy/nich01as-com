f381bb7dc22ca9bab0560fb7dd4cbaf073fc75ba
Nicholas <nicholas.zhaoyu@gmail.com>
2013-11-09 16:11:21 -0800

    Update reading list page.

--- apps/toefl/configs.py
+++ apps/toefl/configs.py
@@ -1,8 +1,10 @@
 from handlers.edit_reading import EditReadingHandler
 from handlers.reading import ReadingHandler
+from handlers.reading import ReadingListHandler
 from handlers.speaking import SpeakingHandler
 
-handlers = [('/toefl/reading', ReadingHandler),
+handlers = [('/toefl/reading', ReadingListHandler),
+            ('/toefl/reading/(tpo.*)', ReadingHandler),
             ('/toefl/speaking', SpeakingHandler),
             ('/toefl/edit_reading', EditReadingHandler)]
 
--- apps/toefl/handlers/reading.py
+++ apps/toefl/handlers/reading.py
@@ -2,34 +2,35 @@ import logging
 
 import handlers
 
-class ReadingHandler(handlers.BasePageHandler):
+class ReadingListHandler(handlers.BasePageHandler):
 
   def get(self):
-    name = self.request.get('name')
-    if name:
-      tpo1_reading = open('apps/toefl/data/reading_%s' % name)
-      reading_lines = tpo1_reading.readlines()
-      title = reading_lines[0].decode('utf-8').strip()
-      tpo1_questions = open('apps/toefl/data/reading_question_%s' % name)
-      question = ''
-      questions = []
-      for question_line in tpo1_questions.readlines():
-        if question_line.isspace():
-          if not question.isspace():
-            questions.append(question.decode('utf-8').strip())
-            question = ''
-        else:
-          question = question + question_line
-      if len(question) > 0:
-        questions.append(question.decode('utf-8').strip())
-      paragraphs = [line.decode('utf-8').strip() for line in reading_lines[1:]]
-      tpo1_reading.close()
-      tpo1_questions.close()
-      self.render('apps/toefl/reading.html', {'title': title, 'paragraphs': paragraphs, 'questions': questions})
-    else:
-      names = []
-      for i in xrange(23):
-        for j in xrange(1, 4):
-          names.append('tpo%d-%d' % (i, j))
-      self.render('apps/toefl/reading_list.html', {'names': names})
-  
+    names = []
+    for i in xrange(1, 25):
+      for j in xrange(1, 4):
+        names.append('tpo%d-%d' % (i, j))
+    self.render('apps/toefl/reading_list.html', {'names': names})
+
+class ReadingHandler(handlers.BasePageHandler):
+
+  def get(self, name):
+    tpo1_reading = open('apps/toefl/data/reading/reading_%s.data' % name)
+    reading_lines = tpo1_reading.readlines()
+    title = reading_lines[0].decode('utf-8').strip()
+    tpo1_questions = open('apps/toefl/data/reading/reading_question_%s.data' % name)
+    question = ''
+    questions = []
+    for question_line in tpo1_questions.readlines():
+      if question_line.isspace():
+        if not question.isspace():
+          questions.append(question.decode('utf-8').strip())
+          question = ''
+      else:
+        question = question + question_line
+    if len(question) > 0:
+      questions.append(question.decode('utf-8').strip())
+    paragraphs = [line.decode('utf-8').strip() for line in reading_lines[1:]]
+    tpo1_reading.close()
+    tpo1_questions.close()
+    self.render('apps/toefl/reading.html', {'title': title, 'paragraphs': paragraphs, 'questions': questions})
+        
