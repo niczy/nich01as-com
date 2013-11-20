3d17dc6f584fcf9853c9bc79c04de9a7385b0411
Nicholas Zhao <nicholaszhao@google.com>
2013-11-20 13:38:05 -0800

    Update.

--- static_dir/scripts/gaia/controllers.js
+++ static_dir/scripts/gaia/controllers.js
@@ -26,7 +26,7 @@ phonecatControllers.controller('Signup3Ctrl',
     ['$scope', '$routeParams', function($scope, $routeParams) {
       $scope.phoneId = $routeParams.phoneId;
     }]);
-phonecatControllers.controller('Signup4Ctrl', 
+phonecatControllers.controller('Signup4Ctrl',
     ['$scope', '$routeParams', function($scope, $routeParams) {
       $scope.phoneId = $routeParams.phoneId;
     }]);
@@ -34,4 +34,60 @@ phonecatControllers.controller('MainCtrl', function ($scope, $location) {
       $scope.go = function (path) {
                 $location.path(path);
       }
+});
+
+phonecatControllers.controller('NavCtrl', function($scope, $rootScope, $location) {
+    var styles = {
+      left: ' \
+        .page.ng-enter { \
+          position:absolute; \
+          -webkit-transition: 10.3s ease-out all; \
+          -webkit-transform:translate3d(100%,0,0) ; \
+        } \
+        .page.ng-enter.ng-enter-active { \
+          position:absolute; \
+          -webkit-transform:translate3d(0,0,0) ; \
+        } \
+        .page.ng-leave { \
+          position:absolute; \
+          -webkit-transition: 10.3s ease-out all; \
+          -webkit-transform:translate3d(0,0,0) ; \
+        } \
+        .page.ng-leave.ng-leave-active { \
+          position:absolute; \
+          -webkit-transform:translate3d(-100%,0,0) ; \
+        }; \
+      ',
+     right: ' \
+        .page.ng-enter { \
+          position:absolute; \
+          -webkit-transition: 10.3s ease-out all; \
+          -webkit-transform:translate3d(-100%,0,0) ; \
+        } \
+        .page.ng-enter.ng-enter-active { \
+          position:absolute; \
+          -webkit-transform:translate3d(0,0,0) ; \
+        } \
+        .page.ng-leave { \
+          position:absolute; \
+          -webkit-transition: 10.3s ease-out all; \
+          -webkit-transform:translate3d(0,0,0) ; \
+        } \
+        .page.ng-leave.ng-leave-active { \
+          position:absolute; \
+          -webkit-transform:translate3d(100%,0,0) ; \
+        }; \
+      ',
+    };
+    $scope.direction = function(dir) {
+      $rootScope.style = styles[dir]
+      console.log($rootScope.style);
+    }
+    $scope.goLast = function (path){
+      $location.path(path);
+    }
+
+    $scope.goNext = function(path) {
+      $location.path(path);
+    }
 });
