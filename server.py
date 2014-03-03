2b38e8ab6f361c6d986735573b16889ded455298
Nicholas <nicholas.zhaoyu@gmail.com>
2014-03-03 10:04:08 -0800

    Fix bug.

--- server.py
+++ server.py
@@ -7,12 +7,11 @@ from handlers.index import IndexHandler
 from handlers.login import LoginHandler
 from handlers.debug import SystemInfoHandler
 from apps.toefl import configs as toefl_config
-from apps.shuwen import configs as shuwen_config
 
 handlers = [('/', IndexHandler), 
             ('/debug', SystemInfoHandler),
             ('/login', LoginHandler)]
-handlers = handlers + toefl_config.handlers + shuwen_config.handlers
+handlers = handlers + toefl_config.handlers
 
 logging.info(handlers)
 
